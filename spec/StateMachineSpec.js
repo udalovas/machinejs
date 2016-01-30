1a2bebe377a4129e82126d0679d517f12bff6e6f
Alexey Udalov <al.udalec@gmail.com>
2016-01-31 00:37:36 +0300

    Transitions setup

--- spec/StateMachineSpec.js
+++ spec/StateMachineSpec.js
@@ -12,13 +12,13 @@ describe("StateMachine", function() {
         expect(StateMachine).toBeDefined();
     });
 
-    describe("States registration", function() {
+    describe("States setup", function() {
 
         it("allows to add states", function() {
             _stateMachine.addState({ id: "stateA" });
         });
 
-        it("indicates if state is added", function() {
+        it("indicates if some state is added", function() {
             _stateMachine.addState({ id: "state A" });
             expect(_stateMachine.hasState("state A")).toBe(true);
         });
@@ -30,6 +30,49 @@ describe("StateMachine", function() {
             expect(_stateMachine.hasState("state A")).toBe(true);
             expect(_stateMachine.hasState("state B")).toBe(true);
         });
+    });
+
+    describe("Transitions setup", function() {
+
+        it('should define transitions', function() {
+            _stateMachine.defineTransition({
+                from: 'A',
+                to: 'B'
+            });
+        });
+
+        describe('should handle invalid inputs', function() {
+
+            it('indicates if transition there is no states for such transition', function() {
+                expect(_stateMachine.defineTransition({
+                   from: 'A',
+                   to: 'B'
+                })).toBe(false);
+            });
+
+            it('indicates if there is already transition defined between states provided', function() {
+
+                _stateMachine.addState({ id: "A" });
+                _stateMachine.addState({ id: "B" });
+
+                expect(_stateMachine.defineTransition({
+                    from: 'A',
+                    to: 'B'
+                })).toBe(true);
+
+                expect(_stateMachine.defineTransition({
+                    from: 'A',
+                    to: 'B'
+                })).toBe(false);
+            });
+        });
+
+    });
+
+    describe('Transitions in action', function() {
+       it('should execute transition', function() {
+
+       });
     });
 
 });
--- src/js/StateMachine.js
+++ src/js/StateMachine.js
@@ -1,10 +1,12 @@
+'use strict';
 
 var _ = require('lodash');
 
 function StateMachine() {
 
     var self = this,
-        _states = [];
+        _states = [],
+        _transitions = [];
 
     self.addState = function(state) {
         _states.push(state);
@@ -13,6 +15,20 @@ function StateMachine() {
 
     self.hasState = function(stateId) {
         return !_.isEmpty(_.find(_states, function(state) { return state.id === stateId }));
+    };
+
+    self.hasTransition = function(from, to) {
+        return !_.isEmpty(_.find(_transitions, function(transition) { return transition.from === from && transition.to === to }));
+    };
+
+    self.defineTransition = function(transition) {
+
+        if(!self.hasState(transition.from) || !self.hasState(transition.to) || self.hasTransition(transition.from, transition.to)) {
+            return false;
+        }
+
+        _transitions.push(transition);
+        return true;
     };
 
     return self;
